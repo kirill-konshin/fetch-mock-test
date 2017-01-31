005c28791ff4f946859544d3bff8ed634659c9f4
Kirill Konshin <kirill.konshin@gmail.com>
2017-01-31 12:01:45 -0800

    Init

---
+++ index.js
@@ -0,0 +1,30 @@
+(function() {
+
+    var fetchMock = require('fetch-mock');
+    var nodeFetch = require('node-fetch');
+
+    var Request = nodeFetch.Request;
+    var fetch = fetchMock.fetchMock;
+
+    fetchMock.once('http://whatever/foo', {
+        body: '{"foo": "bar"}',
+        status: 200,
+        statusText: 'OK',
+        sendAsJson: false
+    }, {
+        method: 'GET'
+    });
+
+    var req = new Request('http://whatever/foo', {
+        method: 'GET'
+    });
+
+    fetch(req).then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        console.log(json);
+    }).catch(function(e) {
+        console.error(e.stack);
+    });
+
+})();
